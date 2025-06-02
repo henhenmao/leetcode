1296275b96a9040683165e5946446b02c5a049ac
henhenmao <henrymao54321@gmail.com>
2025-06-01 20:37:30 -0700

    fixed the

--- stuff/counter.py
+++ stuff/counter.py
@@ -3,20 +3,30 @@ import os
 
 """
 i will use this program to count the number of solutions in total
+i actually have no idea how accurate this is
 """
 
 def count():
-    num_files = 0
+    num_files = -1 # readme.md gets counted
     num_dirs = 0
-    dir = os.getcwd()
-    print(dir)
 
-    for file in os.listdir(dir):
-        if os.path.isdir(file):
-            num_dirs += 1
-            num_files += len(os.listdir(file))
-        else:
+    print(os.getcwd())
+    print(os.path.dirname(os.getcwd()))
+    
+    for dirpath, dirnames, filenames in os.walk(os.path.dirname(os.getcwd())):
+
+        if ".git" in dirnames:
+            dirnames.remove(".git")
+            
+        if "stuff" in dirnames:
+            dirnames.remove("stuff")
+
+        for filename in filenames:
+            print(filename)
             num_files += 1
+        
+        num_dirs += len(dirnames)
+    
     return f"there are probably {num_files} files and {num_dirs} directories in the current directory"
 
 print(count())
