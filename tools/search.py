147e1970fbd6cb9d285d59ae17134448b2d4590c
henhenmao <henrymao54321@gmail.com>
2025-06-03 07:01:54 -0700

    made changes to search.py

--- tools/search.py
+++ tools/search.py
@@ -1,6 +1,7 @@
 
 
 import os
+import sys
 
 """
 i want to be able to search the name of a file and know if it exists and exactly where it is in the repository
@@ -8,6 +9,9 @@ i want to be able to search the name of a file and know if it exists and exactly
 
 
 def search(file):
+    if file == "":
+        print("empty input\n")
+        return
 
     path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
@@ -23,17 +27,24 @@ def search(file):
                 matches.append(f"{filename} found in {dirpath}")
 
     if matches:
-        print("possible matches\n───── ⋆⋅☆⋅⋆ ─────")
+        print(f'possible matches for "{file}"\n────────── ⋆⋅☆⋅⋆ ──────────')
         for match in matches:
-            print(match)
-        print()
+            print(match + "\n")
     else:
-        print("no matches found....")
-        print()
+        print("no matches found....\n")
 
+# take a command line argument if it exists
+# if no command line argument, ask for an input
+# if multiple command line arguments, return matches for each term
 
-inp = input("what are you looking for: ")
-search(inp)
+if len(sys.argv) < 2:
+    inp = input("what are you looking for: ")
+    print()
+    search(inp)
+else:
+    print()
+    for inp in sys.argv:
+        search(inp)
 
 
 
---
+++ tools/test.py
@@ -0,0 +1,11 @@
+
+
+
+import sys
+
+if len(sys.argv) < 2:
+    print("Usage: python3 search.py [input]")
+    sys.exit(1)
+
+user_input = sys.argv[1]
+print(f"You entered: {user_input}")
