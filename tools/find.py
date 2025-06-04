c08564bdb9587e5841d27041dec3fa265cc3610f
henhenmao <henrymao54321@gmail.com>
2025-06-04 05:27:41 -0700

    changed the name of search and find -> find and todo

--- tools/find.py
+++ tools/find.py
@@ -1,77 +1,52 @@
 
+
 import os
-import re
+import sys
 
 """
-run this to locate files that are marked NO, UF, or NC
-too lazy to write it now i'll do it later    
+i want to be able to search the name of a file and know if it exists and exactly where it is in the repository
 """
 
-unfinished_files = []
-non_optimal_files = []
-no_comment_files = []
-divider = "----------------"
 
-def display():
+def search(file):
+    if file == "":
+        print("empty input\n")
+        return
 
-    print()
-
-    if unfinished_files:
-        print(f"UNFINISHED FILES: {len(unfinished_files)}\n{divider}")
-        for file in unfinished_files:
-            print(file)
-    else:
-        print(f"no unfinished files :)")
-
-    print()
-
-    if unfinished_files:
-        print(f"NON-OPTIMAL FILES: {len(non_optimal_files)}\n{divider}")
-        for file in non_optimal_files:
-            print(file)
-    else:
-        print(f"everything is optimal :)")
-
-    print()
-
-    if no_comment_files:
-        print(f"NO COMMENTS FILES: {len(no_comment_files)}\n{divider}")
-        for file in no_comment_files:
-            print(file)
-    else:
-        print(f"everything is commented :)")
-    print()
-
-        
-def findUF():
     path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+
+    matches = []
 
     for dirpath, dirnames, filenames in os.walk(path):
 
-        # skip the git directory (idk why it's there in the first place)
         if ".git" in dirnames:
             dirnames.remove(".git")
-
-        # print(f"Directory: {dirpath}")
+        
         for filename in filenames:
-            name, _ = os.path.splitext(filename) # separates file name and file extension
-            if name.endswith("UF"):
-                unfinished_files.append(filename)
-            elif name.endswith("NO"):
-                non_optimal_files.append(filename)
-            elif name.endswith("NC"):
-                no_comment_files.append(filename)
-    display()
+            if file in filename:
+                matches.append(f"{filename} at {os.path.join(dirpath, filename)}")
+
+    if matches:
+        print(f'possible matches for "{file}"\n────────── ⋆⋅☆⋅⋆ ──────────')
+        for match in matches:
+            print(match)
+        print()
+    else:
+        print("no matches found....\n")
+
+# take a command line argument if it exists
+# if no command line argument, ask for an input
+# if multiple command line arguments, return matches for each term
+
+if len(sys.argv) < 2:
+    inp = input("what are you looking for: ")
+    print()
+    search(inp)
+else:
+    print()
+    for inp in sys.argv:
+        search(inp)
 
 
-
-
-findUF()
-
-
-
-    
-
-    
 
 
