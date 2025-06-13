7a7a25846c923cf14c1024ba7f27c62f4972cb76
henhenmao <henrymao54321@gmail.com>
2025-06-13 11:43:45 -0700

    reverse bits

---
+++ 190_reverse_bits.py
@@ -0,0 +1,23 @@
+
+
+
+"""
+190. Reverse Bits (https://leetcode.com/problems/reverse-bits/?envType=problem-list-v2&envId=oizxjoit)
+The input must be a binary string of length 32
+Follow up: If this function is called many times, how would you optimize it?
+
+- reverseBits(n) takes in n in integer form
+- convert n into binary form with a 32 bit format and reverse the binary string
+- convert the reversed binary string back into a base 10 integer
+
+runtime: O(n) where n is the length of the binary string
+space: O(n)
+"""
+
+
+def reverseBits(n: int) -> int: # n is binary input
+    n = f"{n:032b}"[::-1]
+    return (int(n, 2))
+
+a = 43261596
+print(reverseBits(a))
